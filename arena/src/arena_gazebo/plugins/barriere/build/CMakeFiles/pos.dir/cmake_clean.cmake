d746404fe654382fb36c3f7cac7a0e8d74ff4dc9
Mathias <mathias.roesler@etu.upmc.fr>
2020-01-16 17:33:32 +0100

    Update arena, added plugins for pedestrian and barrier, only the barrier works for now

---
+++ arena/src/arena_gazebo/plugins/barriere/build/CMakeFiles/pos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pos.dir/pos.cc.o"
+  "pos.pdb"
+  "pos"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ arena/src/arena_gazebo/plugins/pieton/build/CMakeFiles/pieton_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pieton_plugin.dir/pieton_plugin.cc.o"
+  "libpieton_plugin.pdb"
+  "libpieton_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pieton_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
